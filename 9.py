f8005b8e5841eb91b42039788883288ef216bdf9
Sébastien Briois <sebriois@free.fr>
2009-11-07 13:27:09 +0000

    Solution to problem 9

--- 9.py
+++ 9.py
@@ -5,3 +5,17 @@
 # 
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
+
+import math
+
+def main():
+  for a in range(1,999):
+    for b in range(2,999):
+      c = 1000 - b - a
+      a2 = math.pow(a,2)
+      b2 = math.pow(b,2)
+      c2 = math.pow(c,2)
+      if a2 + b2 == c2:
+        return a*b*c
+
+print main()
