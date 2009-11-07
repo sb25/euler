40b09f468575fa4f71491fe250b16a48ff8a6d1f
Sébastien Briois <sebriois@free.fr>
2009-11-07 13:26:51 +0000

    Solution to problem 5

--- 5.py
+++ 5.py
@@ -1,3 +1,9 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # 
 # What is the smallest number that is evenly divisible by all of the numbers from 1 to 20?
+from utils import *
+
+ppcm = 1
+for i in range(2, 21):
+  ppcm = PPCM(ppcm, i)
+print ppcm
