969c7d841875256038c744722f36ceb62ffc9a5c
Sébastien Briois <sebriois@free.fr>
2009-11-07 13:28:11 +0000

    Solution to problem 20

--- 20.py
+++ 20.py
@@ -1,3 +1,6 @@
+# coding: utf8
 # n! means n × (n − 1) × ... × 3 × 2 × 1
 # 
 # Find the sum of the digits in the number 100!
+
+print sum(map(int, list(str(eval("*".join(map(str, range(1,101))))))))
