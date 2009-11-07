61f85e6db1ec632c55fb7ff68f07c66c1bdaa888
Sébastien Briois <sebriois@free.fr>
2009-11-07 13:27:57 +0000

    Solution to problem 16

--- 16.py
+++ 16.py
@@ -1,3 +1,7 @@
+# coding: utf8
 # 2^(15) = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 # 
 # What is the sum of the digits of the number 2^(1000)?
+
+import math
+print sum(map(int, list(str(int(math.pow(2, 1000))))))
